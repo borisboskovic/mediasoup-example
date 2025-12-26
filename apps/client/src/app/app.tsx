f601a7a7c9353d2a6bb7358b2fb82f1ad367c172
Boris Bošković <boris.boskovic92@gmail.com>
2025-12-26 10:06:52 +0100

    Using NavLinks instead of radio buttons

--- apps/client/src/app/app.tsx
+++ apps/client/src/app/app.tsx
@@ -1,44 +1,23 @@
 import Broadcast from './components/Broadcast';
 import Watch from './components/Watch';
-import { ChangeEventHandler } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
+import { NavLink, NavLinkRenderProps, useLocation } from 'react-router-dom';
 
 export function App() {
     const { pathname } = useLocation();
-    const navigate = useNavigate();
 
-    const someFunc: ChangeEventHandler<HTMLInputElement> = (event) => {
-        navigate(event.target.value);
+    const getLinkClasses = (props: NavLinkRenderProps) => {
+        return props.isActive ? 'underline font-semibold' : '';
     };
 
     return (
         <>
-            <header className="flex gap-1 justify-center">
-                <label className="cursor-pointer" htmlFor="broadcast">
+            <header className="flex gap-4 justify-center">
+                <NavLink className={getLinkClasses} to="/broadcast">
                     Broadcast
-                </label>
-                <input
-                    type="radio"
-                    id="broadcast"
-                    name="role-select"
-                    value="broadcast"
-                    onChange={someFunc}
-                    className="cursor-pointer"
-                    defaultChecked={pathname === '/broadcast'}
-                />
-                <div className="w-1"></div>
-                <input
-                    type="radio"
-                    id="watch"
-                    name="role-select"
-                    value="watch"
-                    onChange={someFunc}
-                    className="cursor-pointer"
-                    defaultChecked={pathname === '/watch'}
-                />
-                <label className="cursor-pointer" htmlFor="watch">
+                </NavLink>
+                <NavLink className={getLinkClasses} to="/watch">
                     Watch
-                </label>
+                </NavLink>
             </header>
             <main className="p-2">
                 {pathname === '/broadcast' && <Broadcast />}
